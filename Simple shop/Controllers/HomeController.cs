38eca93f2139cadce5d356e8e4c64dec2570e997
Mgaluszka <Mgaluszka@PLWAWUES107>
2017-10-26 16:50:29 +0200

    poprawa connection String

--- Simple shop/Controllers/HomeController.cs
+++ Simple shop/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Simple_shop.DAL;
+using Simple_shop.Models;
+using System.Dynamic;
 
 namespace Simple_shop.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+      
+        private  CourseContext db = new CourseContext();
         public ActionResult Index()
         {
+            Category  Cat = new Category{nameCategory = "ASP>NET MVC", FileNameIcons = "ASPMVC.png", DescCategory = "opis"};
+            db.Categories.Add(Cat);
+            db.SaveChanges();
             return View();
         }
     }
