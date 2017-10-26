ad3dcd5ec095309b66d0a215bf9eeda88ee2eead
Mgaluszka <Mgaluszka@PLWAWUES107>
2017-10-26 15:55:42 +0200

    Dodanie  KursyContext

---
+++ Simple shop/DAL/CourseContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Simple_shop.Models;
+
+namespace Simple_shop.DAL
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext() : base("CoursyContext")
+        {
+            
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Orders> Orderss { get; set; }
+        public DbSet<PositionOrder> PositionOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            // using System.Data.Entity.ModelConfigurations;;
+            //Wyłacza konwensję, która automatycznie twory liczbe mnoga dla nazw tabel w bazie danych
+            // Zamiast Kategorie zostało stowrzona tabela Kateroiesz
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
