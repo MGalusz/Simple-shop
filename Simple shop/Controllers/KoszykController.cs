7a26c82745dd08de1ac58ede473c48b17b99ed91
Mgaluszka <Mgaluszka@PLWAWUES107>
2017-11-14 15:01:09 +0100

    Dodanie koszyka do apk

--- Simple shop/Controllers/KoszykController.cs
+++ Simple shop/Controllers/KoszykController.cs
@@ -3,20 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Simple_shop.DAL;
+using Simple_shop.Infrastructure;
+using Simple_shop.ViewModels;
 
 namespace Simple_shop.Controllers
 {
     public class KoszykController : Controller
     {
+
+        private KoszykMenager koszykMenager;
+
+        private ISessionMenager sessionMenager { get; set; }
+        private KursyContext db ;
+
+
+        public KoszykController()
+        {
+            db =  new KursyContext();
+            sessionMenager =  new SessionMenager();
+            koszykMenager = new KoszykMenager(sessionMenager,db);
+        }
         // GET: Koszyk
         public ActionResult Index()
         {
-            return View();
+            var pozycjeKoszyka = koszykMenager.PobierzKoszyk();
+            var cenaCalkowita = koszykMenager.PobierzWarośćKoszyka();
+            KoszykVM koszykVM = new KoszykVM()
+            {
+                PozycjeKoszyka = pozycjeKoszyka,
+                CenaCalkowita = cenaCalkowita
+            };
+            return View(koszykVM);
         }
 
-        public ActionResult DodajDoKoszyka(string id)
+        public ActionResult DodajDoKoszyka(int id)
         {
-            return View();
+
+            koszykMenager.DodajDoKoszyka(id);
+
+            return RedirectToAction("Index");
+        }
+
+        public int PobierzIloscElemtowKoszyka()
+        {
+            return koszykMenager.PobierzIlośćPozycjiKozyka();
+
+        }
+
+        public ActionResult UsunZKoszyka(int kursId)
+        {
+            int iloscPozycji = koszykMenager.UsuńZKoszyka(kursId);
+            int iloscPozycjiKoszyka = koszykMenager.PobierzIlośćPozycjiKozyka();
+            decimal wartoscKoszyka = koszykMenager.PobierzWarośćKoszyka();
+
+            var wynik = new KoszykUsuwanieVM
+            {
+                IdPozycjiUsuwanej = kursId,
+                IloscPozycjiUsuwanej = iloscPozycji,
+                KoszykCenaCalkowita = wartoscKoszyka,
+                KoszykIloscPozycji = iloscPozycjiKoszyka,
+
+            };
+
+            return Json(wynik);
         }
     }
 }
---
+++ Simple shop/ViewModels/KoszykVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Simple_shop.Models;
+
+namespace Simple_shop.ViewModels
+{
+    public class KoszykVM
+    {
+        public List<PozycjaKoszyka> PozycjeKoszyka {get;set;}
+        public decimal CenaCalkowita { get; set; }
+    }
+}
